91cf29ff2f9c4505b93398fc49acd08331c5118b
Ewin Mathew <79726981+senorpapistas@users.noreply.github.com>
2022-04-03 12:26:18 -0700

    Uploading Code Journal #1 entry #1
    Write a Python program to print out your full name, your preferred pronouns (optional), and two sentences about your favorite movie and your favorite food.

---
+++ cj1-1.py
@@ -0,0 +1,12 @@
+#This program will print out my first & last name, favorite movie, and favorite food
+
+#main function that will print everything out
+def main():
+    '''I used '\n' to print out a newline after every statement
+    This means I could print everything out with one print statement'''
+
+    print("Name: Ewin Mathew\nOne of my favorite movies is the Broly Movie.\nOne of my favorite foods is Beef Chow Fun.")
+
+#this will execute the main function
+if __name__ == '__main__':
+    main()
